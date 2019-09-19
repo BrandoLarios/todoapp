e7fbd4e1d43903db5aff457741d369bce22f6f90
BrandoLarios <brandolarios@outlook.com>
2019-09-19 11:43:18 -0500

    CRUD of lists created without CSS and index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,46 @@
 class ListsController < ApplicationController
+  #Sirve para que la funcion find_list definida al final
+  #solo se aplique a las funciones permitadas o se aplique
+  #de manera general sin incluir funciones definidas
+  before_action :find_list, only: [:show, :edit, :update, :destroy] #Permite solo en esas
+  #before_action :find_list, except: [:index, :new, :create #Niega en estas otras
+  
+  def index
+  end
+  
   def new
     @list = List.new 
-    @list.name = 'Walmart'
-    @list.description = 'Lista de compra para despenza de walmart' 
+  end
+  
+  def create
+    List.create(list_params)
+    #render json: list_params
+  end
+    
+  def edit
+  end
+  
+  def update
+    @list.update(list_params)
+    redirect_to @list
+  end
+  
+  def show
+  end
+  
+  def destroy
+    @list.destroy
+    redirect_to :action => :new
+  end
+  
+  private
+  def list_params
+    params
+      .require(:list)
+      .permit(:name, :description)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #Rutas de listas 
-  get 'list/new', to: 'lists#new'
-  post 'list', to: 'lists#create'
   
+
+  #Rutas de listas 
+  get "/lists/new", to: "lists#new"
+  get "/lists/:id", to: "lists#show"
+  get "/lists/:id/edit", to: "lists#edit"
+  post "/lists", to: "lists#create"
+  patch "/lists/:id", to: "lists#update", as: :list
+  delete "/lists/:id", to: "lists#destroy"
 
 end
